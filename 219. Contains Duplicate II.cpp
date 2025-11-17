9e1e159bf2060b20c192581968cf3c2d39da9278
Anamika Kushwaha <aiml23058@glbitm.ac.in>
2025-11-17 23:26:44 +0530

    219. Contains Duplicate II.cpp
    LeetCode 219

---
+++ 219. Contains Duplicate II.cpp
@@ -0,0 +1,39 @@
+// 219. Contains Duplicate II
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+// Example 2:
+
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+// Example 3:
+
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+// 0 <= k <= 105
+
+// CODE:-
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int , int> lastIndex;
+
+        for(int i=0 ; i<nums.size() ; i++){
+            if(lastIndex.count(nums[i])){
+                if (i - lastIndex[nums[i]] <= k)
+                    return true;
+            }
+            lastIndex[nums[i]] = i;
+        }
+        return false;
+    }
+};
