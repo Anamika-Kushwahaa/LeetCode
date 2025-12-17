c4049657f98a2414e4c9152f183b941319e8048e
Anamika Kushwaha <aiml23058@glbitm.ac.in>
2025-12-17 23:33:29 +0530

    416. Partition Equal Subset Sum.cpp
    LeetCode 416

---
+++ 416. Partition Equal Subset Sum.cpp
@@ -0,0 +1,44 @@
+// 416. Partition Equal Subset Sum
+// Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,5,11,5]
+// Output: true
+// Explanation: The array can be partitioned as [1, 5, 5] and [11].
+// Example 2:
+
+// Input: nums = [1,2,3,5]
+// Output: false
+// Explanation: The array cannot be partitioned into equal sum subsets.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 200
+// 1 <= nums[i] <= 100
+
+
+//CODE
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for (int x : nums) sum += x;
+
+        if (sum % 2 != 0) return false;
+
+        int target = sum / 2;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+        return dp[target];
+    }
+};
